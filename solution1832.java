6ef3f39f0ecf6dd65be756a641963abe2b7cbbdf
vrishketumishra <mishrarishi2006@gmail.com>
2025-01-02 23:46:03 +0530

    Add files via upload

---
+++ solution1832.java
@@ -0,0 +1,14 @@
+public class solution1832 {
+    public boolean checkIfPangram(String sentence) {
+        boolean[] vis = new boolean[26];
+        for (int i = 0; i < sentence.length(); ++i) {
+            vis[sentence.charAt(i) - 'a'] = true;
+        }
+        for (boolean v : vis) {
+            if (!v) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
