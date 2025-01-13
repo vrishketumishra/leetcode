05e47e205dc7d14a343fcd15a1c14eb2b8938ac2
vrishketumishra <mishrarishi2006@gmail.com>
2025-01-13 22:57:15 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class array {
+    public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);
+        int[][] arr= new int[3][3];
+        System.out.println(arr.length);
+        for(int row=0;row<arr.length;row++){
+            for(int col=0;col<arr[row].length;col++){
+                arr[row][col]=in.nextInt();
+            }
+        }
+
+        for(int row=0;row<arr.length;row++){
+            for(int col=0;col<arr[row].length;col++){
+                System.out.print(arr[row][col] +" ");
+            }
+            System.out.println();
+        } 
+    }
+}
