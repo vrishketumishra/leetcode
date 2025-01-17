208e687bf113705ff5b4b5803ee1c6b8fd2dcfac
vrishketumishra <mishrarishi2006@gmail.com>
2025-01-17 22:31:21 +0530

    Add files via upload

---
+++ MultiAl.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class MultiAl {
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        //initialistation
+        for (int i = 0; i < 3; i++) {
+            list.add(new ArrayList<>());
+        }
+        //add elements
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                //list.get(i).add( in.nextInt());
+            }
+        }
+        System.out.println(list);
+    }
+}
