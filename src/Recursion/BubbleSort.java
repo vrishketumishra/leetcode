c1563cf1e43dca326d639a9b3f0cde4d33b34360
vrishketumishra <mishrarishi2006@gmail.com>
2026-04-30 16:14:58 +0530

    created some recursion files

--- src/Recursion/BubbleSort.java
+++ src/Recursion/BubbleSort.java
@@ -10,8 +10,9 @@ public class BubbleSort {
     static void bubble(int[] arr, int r, int c) {
         if (r == 0) {
             return;
-
-            if (arr[c] > arr[c + 1]) {
+        }
+        if(c<r){
+            if(arr[c] > arr[c + 1]) {
                 //swap
                 int temp = arr[c];
                 arr[c] = arr[c + 1];
